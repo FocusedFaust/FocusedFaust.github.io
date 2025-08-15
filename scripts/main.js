d926e50df114c621fe3266f0f3f6a7f1b2fe70d0
Faust <112011893+FocusedFaust@users.noreply.github.com>
2025-08-15 19:41:49 +0200

    [NEW] Script for the dropdown menu and back-to-top button

---
+++ scripts/main.js
@@ -0,0 +1,20 @@
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function (event) {
+  if (!event.target.matches('#menuBtn')) {
+    var dropdown = document.getElementById("dropdownMenu")
+    if (dropdown.classList.contains('show')) {
+      dropdown.classList.remove('show');
+    }
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+// Display the menu when clickig the menu button
+function displayMenu() {
+  document.getElementById("dropdownMenu").classList.toggle("show")
+}
