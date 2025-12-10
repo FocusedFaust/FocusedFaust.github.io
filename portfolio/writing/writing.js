4d27b40619c16d5f56ccfd6c66a20e6b8641eb7b
FocusedFaust <quieto297@gmail.com>
2025-12-10 11:31:21 +0100

    [STYLE] Style the poems display. Title is now in italics and background is rounded, bordered, and the colour depends on the theme.

--- portfolio/writing/writing.js
+++ portfolio/writing/writing.js
@@ -4,15 +4,16 @@ var display = document.getElementById("display-poems");
 
 for (const x in poems) {
     var cont = document.createElement("p")
-    cont.appendChild(document.createTextNode(x));
+    var span = document.createElement("span"); // The title gets special treatment
+    span.style = "font-style: italic; display: block;"; // is in italics with a margin
+    span.appendChild(document.createTextNode(x));
+    cont.appendChild(span);
     cont.appendChild(document.createElement("br"));
-    for (const line in poems[x]) {
+    for (const line in poems[x]) { // add every line of the poem to the display
         var p = document.createTextNode(poems[x][line]);
         cont.appendChild(p);
         var br = document.createElement("br");
         cont.appendChild(br);
     }
-    //display.appendChild(document.createTextNode(""));
-    //display.appendChild(document.createElement("br"));
     display.appendChild(cont);
 }
