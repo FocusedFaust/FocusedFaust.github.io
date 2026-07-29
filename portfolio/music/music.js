8b22bb6fabd0592c0d575e969919df7ed72eea37
FocusedFaust <quieto297@gmail.com>
2026-07-29 15:11:35 +0200

    [FEAT] Add progress bar and store duration in entries file

--- portfolio/music/music.js
+++ portfolio/music/music.js
@@ -20,32 +20,45 @@ for (const song in music) {
     audio.id = "song-audio"
     audio.setAttribute("src", music[song]["file"]);
     //audio.setAttribute("controls", "controls");
+    audio.addEventListener("timeupdate", function() {
+        var thisProgress = this.parentElement.querySelector("#song-progress");
+        thisProgress.value = this.currentTime;
+    })
 
+    var controls = document.createElement("div");
     var playButton = document.createElement("button");
-    playButton.innerHTML = "Play";
+    playButton.style = "background: none; border: none; cursor: pointer;";
+    playButton.innerHTML = "&#9654";
     // Event listener for the play/pause button
     playButton.addEventListener("click", function() {
-        var audio = this.parentElement.parentElement.children.namedItem("song-audio");
-        if (audio.paused == true) {
+        var thisAudio = this.parentElement.parentElement.parentElement.children.namedItem("song-audio");
+        if (thisAudio.paused == true) {
             // Play the audio
-            audio.play();
+            thisAudio.play();
             // Update the button text to 'Pause'
-            this.innerHTML = "Pause";
+            this.innerHTML = "&#9615 &#9615";
         } else {
             // Pause the audio
-            audio.pause();
+            thisAudio.pause();
             // Update the button text to 'Play'
-            this.innerHTML = "Play";
+            this.innerHTML = "&#9654";
         }
     });
 
-    text.appendChild(playButton);
+    var songTime = document.createElement("progress");
+    songTime.id = "song-progress";
+    songTime.max = music[song]["duration"];
+    songTime.value = audio.currentTime;
+
+    controls.appendChild(playButton);
+    controls.appendChild(songTime);
+    text.appendChild(controls);
     cont.appendChild(image);
     cont.appendChild(audio);
     cont.appendChild(text);
     display.appendChild(cont);
 
-    //TODO: Make play button pause all other songs
+    //TODO: Make play button pause all other songs (?)
     //TODO: Make play button rotate cd drawing
     //TODO: Style the song display + move hardcoded style to css file
 }
